be5bdcc738b512c92ee4c9717cba0a5af970afd8
Jozue06 <jozue06@gmail.com>
2018-05-17 19:17:59 -0700

    starting, section 1 complete

---
+++ lab-02-josh/lib/list.js
@@ -0,0 +1,16 @@
+'use strict';
+
+class List {
+
+  constructor() {
+    this.length = 0;
+  }
+
+  push(item) {
+    this[this.length++] = item;
+    return this.length;
+  }
+
+}
+
+module.exports = List;
